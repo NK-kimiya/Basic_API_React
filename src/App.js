5ee51ee782b38f1580e14871dd01d20e1595a1a4
NK-kimiya <kinarishige26@gmail.com>
2025-03-07 23:04:53 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import DrfApiFetch from './components/DrfApiFetch';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DrfApiFetch />
+      </header>
+    </div>
+  );
+}
+
+export default App;
